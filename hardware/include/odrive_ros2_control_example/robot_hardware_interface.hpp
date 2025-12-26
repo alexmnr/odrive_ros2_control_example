28b459ebb86e7f19ca0ed0f4a60c4edbf128caaa
alexmnr <alexander@minor.pw>
2025-12-26 01:25:26 +0000

    socket can config and read state done

--- hardware/include/odrive_ros2_control_example/robot_hardware_interface.hpp
+++ hardware/include/odrive_ros2_control_example/robot_hardware_interface.hpp
@@ -12,25 +12,52 @@
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+#include "socket_can.hpp"
+#include "can_helpers.hpp"
+#include "can_simple_messages.hpp"
+#include "odrive_enums.h"
 
 namespace odrive_ros2_control_example
 {
 class RobotHardwareInterface : public hardware_interface::SystemInterface
 {
   struct Joint {
-    std::string name;
-    int can_id;
-    double reduction_ratio;
-    double velocity_limit;
-    double effort_limit;
+    // parameters
+    const std::string name;
+    const uint8_t can_id;
+    const double reduction_ratio;
+    const double velocity_limit;
+    const double effort_limit;
+    // command variables
     double position_command = 0.0;
     double velocity_command = 0.0;
     double effort_command = 0.0;
+    // state variables
     double position_state = 0.0;
     double velocity_state = 0.0;
     double effort_state = 0.0;
-    Joint(std::string name_, int can_id_, double reduction_ratio_, double velocity_limit_, double effort_limit_) 
+    double effort_target = 0.0;
+    // Constructor Function
+    Joint(std::string name_, uint8_t can_id_, double reduction_ratio_, double velocity_limit_, double effort_limit_) 
       : name(name_), can_id(can_id_), reduction_ratio(reduction_ratio_), velocity_limit(velocity_limit_), effort_limit(effort_limit_) {}
+    // CAN variables
+    SocketCanIntf* can_intf;
+    // functions
+    void on_can_msg(const can_frame& frame);
+    template <typename T>
+    void send(const T& msg, bool rtr = false) const {
+        struct can_frame frame;
+        frame.can_id = can_id << 5 | msg.cmd_id;
+        if (rtr) {
+          frame.can_id |= CAN_RTR_FLAG;
+          frame.can_dlc = 0;
+        } else {
+          frame.can_dlc = msg.msg_length;
+          msg.encode_buf(frame.data);
+        }
+
+        can_intf->send_can_frame(frame);
+    }
   };
 
 public:
@@ -47,9 +74,13 @@ public:
 
 
 private:
-  int debug_;
+  std::string can_interface_name_;
+  SocketCanIntf can_intf_;
+  EpollEventLoop event_loop_;
 
   std::vector<Joint> joints;
+
+  void on_can_msg(const can_frame& frame);
 };
 
 }  // namespace odrive_ros2_control_example
--- hardware/robot_hardware_interface.cpp
+++ hardware/robot_hardware_interface.cpp
@@ -1,13 +1,12 @@
 #include "odrive_ros2_control_example/robot_hardware_interface.hpp"
 
-#include <chrono>
 #include <cmath>
-#include <iomanip>
-#include <limits>
-#include <memory>
-#include <sstream>
 #include <string>
 #include <vector>
+#include "socket_can.hpp"
+#include "can_helpers.hpp"
+#include "can_simple_messages.hpp"
+#include "odrive_enums.h"
 
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -16,115 +15,159 @@ namespace odrive_ros2_control_example
 {
   ////////////////////// on_init /////////////////////////
   hardware_interface::CallbackReturn RobotHardwareInterface::on_init(const hardware_interface::HardwareComponentInterfaceParams & params) {
-    RCLCPP_INFO(get_logger(), "Initializing ...please wait...");
     if (hardware_interface::SystemInterface::on_init(params) != hardware_interface::CallbackReturn::SUCCESS) {
       return hardware_interface::CallbackReturn::ERROR;
     }
 
-    debug_ = (int)stod(info_.hardware_parameters["debug"]);
-    RCLCPP_INFO(get_logger(), "Debug: %d", debug_);
+    can_interface_name_ = info_.hardware_parameters["can_interface_name"];
+
+    RCLCPP_INFO(rclcpp::get_logger("RobotHardwareInterface"), "CAN inteface name: %s", can_interface_name_.c_str());
 
     for (const hardware_interface::ComponentInfo & joint_info : info_.joints)
     {
       // Check for correct size of command and state interfaces
       if (joint_info.command_interfaces.size() != 3) {
-        RCLCPP_FATAL(get_logger(), "Joint '%s' has %zu command interfaces. 3 expected.", joint_info.name.c_str(), joint_info.command_interfaces.size());
+        RCLCPP_FATAL(rclcpp::get_logger("RobotHardwareInterface"), "Joint '%s' has %zu command interfaces. 3 expected.", joint_info.name.c_str(), joint_info.command_interfaces.size());
         return hardware_interface::CallbackReturn::ERROR;
       }
       if (joint_info.state_interfaces.size() != 3) {
-        RCLCPP_FATAL(get_logger(), "Joint '%s' has %zu state interfaces. 3 expected.", joint_info.name.c_str(), joint_info.state_interfaces.size());
+        RCLCPP_FATAL(rclcpp::get_logger("RobotHardwareInterface"), "Joint '%s' has %zu state interfaces. 3 expected.", joint_info.name.c_str(), joint_info.state_interfaces.size());
         return hardware_interface::CallbackReturn::ERROR;
       }
 
       // create joint object
       std::string name_ = joint_info.name;
-      int can_id_ = std::stoi(joint_info.parameters.at("can_id"));
+      int can_id_ = (uint8_t)std::stoi(joint_info.parameters.at("can_id"));
       double reduction_ratio_ = std::stod(joint_info.parameters.at("reduction_ratio"));
       double velocity_limit_ = std::stod(joint_info.parameters.at("velocity_limit"));
       double effort_limit_ = std::stod(joint_info.parameters.at("effort_limit"));
       joints.push_back(Joint(name_, can_id_, reduction_ratio_, velocity_limit_, effort_limit_));
+      RCLCPP_INFO(rclcpp::get_logger("RobotHardwareInterface"), "Initialized Joint '%s' with can_id: %d reduction: %.1f velocity_limit: %.1f effort_limit: %.1f", name_.c_str(), can_id_, reduction_ratio_, velocity_limit_, effort_limit_);
     }
 
-    RCLCPP_INFO(get_logger(), "Successfully initialized!");
     return hardware_interface::CallbackReturn::SUCCESS;
   }
 
   ////////////////////// on_configure /////////////////////////
   hardware_interface::CallbackReturn RobotHardwareInterface::on_configure(const rclcpp_lifecycle::State &) {
-    RCLCPP_INFO(get_logger(), "Configuring ...please wait...");
-    RCLCPP_INFO(get_logger(), "Successfully configure!");
+    if (!can_intf_.init(can_interface_name_, &event_loop_, std::bind(&RobotHardwareInterface::on_can_msg, this, _1))) {
+      RCLCPP_ERROR(
+          rclcpp::get_logger("ODriveHardwareInterface"),
+          "Failed to initialize SocketCAN on %s",
+          can_interface_name_.c_str()
+          );
+      return CallbackReturn::ERROR;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("RobotHardwareInterface"), "Initialized SocketCAN on %s", can_interface_name_.c_str());
+    for (auto& joint : joints) {
+      joint.can_intf = &can_intf_;
+    }
+
     return hardware_interface::CallbackReturn::SUCCESS;
   }
 
   ////////////////////// on_activate /////////////////////////
   hardware_interface::CallbackReturn RobotHardwareInterface::on_activate(const rclcpp_lifecycle::State &) {
-    RCLCPP_INFO(get_logger(), "Activating ...please wait...");
-    RCLCPP_INFO(get_logger(), "Successfully activated!");
     return hardware_interface::CallbackReturn::SUCCESS;
   }
 
 
   ////////////////////// read /////////////////////////
   hardware_interface::return_type RobotHardwareInterface::read(const rclcpp::Time &, const rclcpp::Duration &) {
-    // for (auto &joint : joints) {
-      // joint.position_state = joint.position_command;
-      // joint.velocity_state = joint.velocity_command;
-      // joint.effort_state = joint.effort_command;
-      // RCLCPP_INFO(get_logger(), "Read: Position: %f Velocity: %f Effort: %f", joint.position_state, joint.velocity_state, joint.effort_state);
-    // }
+    while (can_intf_.read_nonblocking()) {
+      // repeat until CAN interface has no more messages
+    }
+    // Request Torques and Encoder Estimates
+    for (auto &joint : joints) {
+      Get_Torques_msg_t get_torques_msg;
+      Get_Encoder_Estimates_msg_t get_encoder_estimages_msg;
+      joint.send(get_torques_msg, true);
+      joint.send(get_encoder_estimages_msg, true);
+    }
+
     return hardware_interface::return_type::OK;
   }
 
   ////////////////////// write /////////////////////////
   hardware_interface::return_type RobotHardwareInterface::write(const rclcpp::Time &, const rclcpp::Duration &) {
     // for (auto &joint : joints) {
-      // RCLCPP_INFO(get_logger(), "Write: Position: %f Velocity: %f Effort: %f", joint.position_command, joint.velocity_command, joint.effort_command);
+    // RCLCPP_INFO(rclcpp::get_logger("RobotHardwareInterface"), "Write: Position: %f Velocity: %f Effort: %f", joint.position_command, joint.velocity_command, joint.effort_command);
     // }
     return hardware_interface::return_type::OK;
   }
 
   ////////////////////// on_deactivate /////////////////////////
   hardware_interface::CallbackReturn RobotHardwareInterface::on_deactivate(const rclcpp_lifecycle::State &) {
-    RCLCPP_INFO(get_logger(), "Deactivating ...please wait...");
-    RCLCPP_INFO(get_logger(), "Successfully deactivated!");
     return hardware_interface::CallbackReturn::SUCCESS;
   }
-  
+
   ////////////////////// on_cleanup /////////////////////////
   hardware_interface::CallbackReturn RobotHardwareInterface::on_cleanup(const rclcpp_lifecycle::State &) {
-    RCLCPP_INFO(get_logger(), "Cleaning up ...please wait...");
-    RCLCPP_INFO(get_logger(), "Successfully cleaned up!");
     return hardware_interface::CallbackReturn::SUCCESS;
   }
 
   ////////////////////// on_shutdown /////////////////////////
   hardware_interface::CallbackReturn RobotHardwareInterface::on_shutdown(const rclcpp_lifecycle::State &) {
-    RCLCPP_INFO(get_logger(), "Shutting down ...please wait...");
-    RCLCPP_INFO(get_logger(), "Successfully shut down!");
     return hardware_interface::CallbackReturn::SUCCESS;
   }
 
   ////////////////////// export_state_interfaces /////////////////////////
   std::vector<hardware_interface::StateInterface> RobotHardwareInterface::export_state_interfaces() {
-      std::vector<hardware_interface::StateInterface> state_interfaces;
-      for (auto &joint : joints) {
-        state_interfaces.emplace_back(joint.name, "position", &joint.position_command);
-        state_interfaces.emplace_back(joint.name, "velocity", &joint.velocity_command);
-        state_interfaces.emplace_back(joint.name, "effort", &joint.effort_command);
-      }
-      return state_interfaces;
+    std::vector<hardware_interface::StateInterface> state_interfaces;
+    for (auto &joint : joints) {
+      state_interfaces.emplace_back(joint.name, "position", &joint.position_state);
+      state_interfaces.emplace_back(joint.name, "velocity", &joint.velocity_state);
+      state_interfaces.emplace_back(joint.name, "effort", &joint.effort_state);
     }
+    return state_interfaces;
+  }
 
   ////////////////////// export_command_interfaces /////////////////////////
   std::vector<hardware_interface::CommandInterface> RobotHardwareInterface::export_command_interfaces() {
-      std::vector<hardware_interface::CommandInterface> command_interfaces;
-      for (auto &joint : joints) {
-        command_interfaces.emplace_back(joint.name, "position", &joint.position_command);
-        command_interfaces.emplace_back(joint.name, "velocity", &joint.velocity_command);
-        command_interfaces.emplace_back(joint.name, "effort", &joint.effort_command);
-      }
-      return command_interfaces;
+    std::vector<hardware_interface::CommandInterface> command_interfaces;
+    for (auto &joint : joints) {
+      command_interfaces.emplace_back(joint.name, "position", &joint.position_command);
+      command_interfaces.emplace_back(joint.name, "velocity", &joint.velocity_command);
+      command_interfaces.emplace_back(joint.name, "effort", &joint.effort_command);
     }
+    return command_interfaces;
+  }
+
+  void RobotHardwareInterface::on_can_msg(const can_frame& frame) {
+    for (auto& joint : joints) {
+      if ((frame.can_id >> 5) == joint.can_id) {
+        joint.on_can_msg(frame);
+      }
+    }
+  }
+
+  void RobotHardwareInterface::Joint::on_can_msg(const can_frame& frame) {
+    uint8_t cmd = frame.can_id & 0x1f;
+
+    auto try_decode = [&]<typename TMsg>(TMsg& msg) {
+      if (frame.can_dlc < Get_Encoder_Estimates_msg_t::msg_length) {
+        RCLCPP_WARN(rclcpp::get_logger("RobotHardwareInterface"), "message %d too short", cmd);
+        return false;
+      }
+      msg.decode_buf(frame.data);
+      return true;
+    };
+
+    switch (cmd) {
+      case Get_Encoder_Estimates_msg_t::cmd_id: {
+        if (Get_Encoder_Estimates_msg_t msg; try_decode(msg)) {
+          position_state = msg.Pos_Estimate * (2 * M_PI);
+          velocity_state = msg.Vel_Estimate * (2 * M_PI);
+        }
+      } break;
+      case Get_Torques_msg_t::cmd_id: {
+        if (Get_Torques_msg_t msg; try_decode(msg)) {
+          effort_target = msg.Torque_Target;
+          effort_state = msg.Torque_Estimate;
+        }
+      } break;
+    }
+  }
 
 
 }  // namespace odrive_ros2_control_example
